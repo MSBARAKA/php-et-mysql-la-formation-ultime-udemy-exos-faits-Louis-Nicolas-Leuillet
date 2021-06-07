c56bf70076b4b95e6a8e28bcd43c6a04ef9ee48e
MSBARAKA <75018405+MSBARAKA@users.noreply.github.com>
2021-06-07 17:03:00 +0200

    Add files via upload
    calculs et variables

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$direBonjour = "Hello";
+$destination = "World";
+
+echo $direBonjour.' '.$destination. '  comment allez vous ?' ;
+
+
+
+?>
+
+<?php  
+
+$premierNombre = "-159";
+$deuxiemeNombre = "666";
+
+
+echo $premierNombre * $deuxiemeNombre;
+
+
+?>
